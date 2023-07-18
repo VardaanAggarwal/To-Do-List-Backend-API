da8c733c26682c520ceddb63cfb3ec22eb64c592
Vardaan Aggarwal <98269336+VardaanAggarwal@users.noreply.github.com>
2023-07-18 11:28:45 +0530

    get todo by id

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ let todos = [];
 app.get("/todos", (req, res) => {
   res.status(200).send(todos);
 });
+app.get("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find((todo) => todo.id === id);
+  // console.log(todo);
+  // console.log(id);
+  if (todo) {
+    res.status(200).json(todo);
+  } else {
+    res.status(404).json({
+      error: "To do not found.",
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Your app listening on port ${PORT}`);
